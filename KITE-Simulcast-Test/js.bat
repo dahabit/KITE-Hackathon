579ea2fa5e33f4043cb3673b0252a146ddf2aea6
ManuCosmo <emmanuel.andre@cosmosoftware.io>
2019-03-24 10:01:10 +0100

    Maven to trigger npm install on KITE-Common/js

--- KITE-Simulcast-Test/js.bat
+++ KITE-Simulcast-Test/js.bat
@@ -5,5 +5,4 @@ IF "%1"=="h264" (
  	java -cp ../KITE-Engine/target/kite-jar-with-dependencies.jar;../KITE-Common/target/kite-common-1.0-SNAPSHOT.jar;../KITE-Engine-IF/target/kite-if-1.0-SNAPSHOT.jar;../KITE-Engine/target/kite-engine-1.0-SNAPSHOT.jar;target/Janus-test-1.0-SNAPSHOT.jar org.webrtc.kite.Engine configs/js.vp8.medooze.config.json
 ) else (
 	java -cp ../KITE-Engine/target/kite-jar-with-dependencies.jar;../KITE-Common/target/kite-common-1.0-SNAPSHOT.jar;../KITE-Engine-IF/target/kite-if-1.0-SNAPSHOT.jar;../KITE-Engine/target/kite-engine-1.0-SNAPSHOT.jar;target/Janus-test-1.0-SNAPSHOT.jar org.webrtc.kite.Engine configs/js.config.json
-
 )
